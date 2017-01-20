433aab491d38c5f731c46e99df81b4d4eaa5d130
INTERLOGIC\ora <ora@interlogic.com.ua>
2017-01-20 11:13:07 +0200

    init commit

---
+++ WpfApplication1/WpfApplication1/folder1/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.folder1
+{
+    class Class1
+    {
+        int i3;
+        int i1
+        {
+            get;
+            set;
+        }
+        int i2;
+    }
+}
